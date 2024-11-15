a7a31245dc83a37beeacd747f433523613811875
Vaibhav Pradeep <Vaibhavpradeep777@gmail.com>
2024-11-15 15:50:00 -0500

    Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HumanPlayerYawAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HumanPlayerYawAuto.java
@@ -1,4 +1,31 @@
 package org.firstinspires.ftc.teamcode;
 
-public class HumanPlayerYawAuto {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+@Autonomous( name = "Basketism")
+public class HumanPlayerYawAuto extends OpMode {
+
+    DriveTrainYawMethods auto = new DriveTrainYawMethods();
+    IntakeLiftCameraAuto ILCA = new IntakeLiftCameraAuto();
+
+    final double speed = 0.6;
+
+    @Override
+    public void init() {
+        auto.initDrivetrainYaw(hardwareMap);
+        ILCA.initILCA(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+
+        // move forward to basket
+        auto.drive(speed, 23);
+
+        // turn around
+        auto.turn(speed, 180, "left");
+
+        //go up a bit
+
+    }
 }
