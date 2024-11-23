adb03564fdb08c8c828cd69e4d3e75a171dbacee
Vaibhav Pradeep <Vaibhavpradeep777@gmail.com>
2024-11-23 11:09:05 -0500

    Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCDashboard.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCDashboard.java
@@ -1,17 +1,44 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 @Config
-@TeleOp(name = "FTCDashboard", group = "Linear")
+@TeleOp(name = "Servo Tester", group = "Testing")
 public class FTCDashboard extends LinearOpMode {
 
-    @Override
-    public void runOpMode() throws InterruptedException {
+    // Declare the servo
+    public static String servoName = "Servo1"; // Name of the servo in the config
+    public static double servoPosition = 0;    // Initial servo position (range 0 to 1)
 
+    private Servo Servo1;
+
+    @Override
+    public void runOpMode() {
+        // Initialize servo
+        Servo1 = hardwareMap.get(Servo.class, servoName);
+
+        // Initialize FTC Dashboard
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+
+        telemetry.addLine("Ready to start");
+        telemetry.update();
+
+        // Wait for start command
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // Set servo position
+            Servo1.setPosition(servoPosition);
+
+            // Send telemetry to dashboard
+            telemetry.addData("Servo Name", servoName);
+            telemetry.addData("Servo Position", servoPosition);
+            telemetry.update();
+        }
     }
 }
+
