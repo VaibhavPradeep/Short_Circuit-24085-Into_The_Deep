3629a618e016c6cf782f1e9b1af4f0cafbd4b47c
Short Circuit <Shuppada@gmail.com>
2025-01-17 15:24:53 -0500

    Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMeet3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMeet3.java
@@ -1,7 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BHI260IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @TeleOp(name = "TeleOpMeet3")
 public class TeleOpMeet3 extends LinearOpMode {
@@ -12,6 +17,7 @@ public class TeleOpMeet3 extends LinearOpMode {
     boolean aAlreadyPressed;
     boolean motorOn;
 
+    /*
     public void driveMecanum(double left_y, double left_x, double right_x, double left_trigger){
         double maxPower = Math.max(Math.abs(left_y) + Math.abs(left_x) + Math.abs(right_x), 0.8);
         drivetrain.frontLeft.setPower((left_y - left_x - right_x) / maxPower);
@@ -19,6 +25,9 @@ public class TeleOpMeet3 extends LinearOpMode {
         drivetrain.backLeft.setPower((left_y + left_x - right_x) / maxPower);
         drivetrain.backRight.setPower((left_y - left_x + right_x) / maxPower);
     }
+
+     */
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -33,13 +42,30 @@ public class TeleOpMeet3 extends LinearOpMode {
         ILC.clawServo.setPosition(0.4);
         ILC.intakeClawServo.setPosition(0);
 
+        BHI260IMU imu;
+        BHI260IMU.Parameters parameters;
+        YawPitchRollAngles angles;
+
+        parameters = new BHI260IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                        RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+                )
+        );
+
+        imu = hardwareMap.get(BHI260IMU.class, "imu");
+        imu.initialize(parameters);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
         waitForStart();
 
         while(opModeIsActive()) {
             telemetry.update();
 
             // drivetrain
-            driveMecanum(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.left_trigger);
+            //driveMecanum(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.left_trigger);
 
             // double reverse four bar
             // 215 dr4b, 0.5 outake bar
@@ -61,6 +87,37 @@ public class TeleOpMeet3 extends LinearOpMode {
             }
 
              */
+            double y = -gamepad1.left_stick_y; // CHANGE NEG OR POS
+            double x = gamepad1.left_stick_x * 1.1; // CHANGE NEG OR POS
+            double rx = gamepad1.right_stick_x; // rotating
+
+            angles = imu.getRobotYawPitchRollAngles();
+            double yaw = angles.getYaw(AngleUnit.RADIANS); // robot heading
+            double pitch = angles.getPitch(AngleUnit.RADIANS);
+            double roll = angles.getRoll(AngleUnit.RADIANS);
+
+            double rotatedX = x * Math.cos(yaw) - y * Math.sin(yaw);
+            double rotatedY = x * Math.sin(yaw) + y * Math.cos(yaw);
+
+            double denominator = Math.max(Math.abs(rotatedY) + Math.abs(rotatedX) + Math.abs(rx), 1.0);
+            double frontLeftPower = (rotatedY + rotatedX + rx) / denominator;
+            double backLeftPower = (rotatedY - rotatedX + rx) / denominator;
+            double frontRightPower = (rotatedY - rotatedX - rx) / denominator;
+            double backRightPower = (rotatedY + rotatedX - rx) / denominator;
+
+            drivetrain.frontLeft.setPower(frontLeftPower);
+            drivetrain.backLeft.setPower(backLeftPower);
+            drivetrain.frontRight.setPower(frontRightPower);
+            drivetrain.backRight.setPower(backRightPower);
+
+            telemetry.addData("Yaw (radians)", yaw);
+            telemetry.addData("Pitch (radians)", pitch);
+            telemetry.addData("Roll (radians)", roll);
+            telemetry.addData("FL Power", frontLeftPower);
+            telemetry.addData("BL Power", backLeftPower);
+            telemetry.addData("FR Power", frontRightPower);
+            telemetry.addData("BR Power", backRightPower);
+            telemetry.update();
 
             if(gamepad2.dpad_up) {
                 ILC.dPadMove("up");
