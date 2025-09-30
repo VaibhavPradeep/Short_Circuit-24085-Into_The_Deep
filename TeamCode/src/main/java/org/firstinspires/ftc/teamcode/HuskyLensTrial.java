08f24ec532109f8b802394f5a613693b62b5e8bb
Vaibhav Pradeep <Vaibhavpradeep777@gmail.com>
2025-09-30 16:00:37 -0400

    Changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HuskyLensTrial.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.dfrobot.HuskyLens;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+
+import java.util.concurrent.TimeUnit;
+
+@TeleOp(name = "HuskyLensTrial_Iterative")
+public class HuskyLensTrial extends OpMode {
+    /* tag id 1: GPP
+    2: PGP
+    3: PPG
+     */
+    private final int READ_PERIOD = 1;
+
+    private HuskyLens huskyLens;
+    private Deadline rateLimit;
+
+    boolean motif1 = false;
+    boolean motif2 = false;
+    boolean motif3 = false;
+
+    @Override
+    public void init() {
+        huskyLens = hardwareMap.get(HuskyLens.class, "huskylens");
+
+        // Rate limiter for telemetry
+        rateLimit = new Deadline(READ_PERIOD, TimeUnit.SECONDS);
+        rateLimit.expire();
+
+        if (!huskyLens.knock()) {
+            telemetry.addData(">>", "Problem communicating with " + huskyLens.getDeviceName());
+        } else {
+            telemetry.addData(">>", "Initialized successfully. Ready to start!");
+        }
+
+        // Choose the algorithm
+        huskyLens.selectAlgorithm(HuskyLens.Algorithm.TAG_RECOGNITION);
+        // huskyLens.selectAlgorithm(HuskyLens.Algorithm.COLOR_RECOGNITION);
+    }
+
+    @Override
+    public void loop() {
+        if (!rateLimit.hasExpired()) {
+            return;
+        }
+        rateLimit.reset();
+
+        // Get blocks (recognized objects)
+        HuskyLens.Block[] blocks = huskyLens.blocks();
+        telemetry.addData("Block count", blocks.length);
+
+        for (int i = 0; i < blocks.length; i++) {
+            telemetry.addData("Block", blocks[i].toString());
+            if (blocks[i].id == 1) {
+                motif1 = true;
+            } else if (blocks[i].id == 2) {
+                motif2 = true;
+            } else if (blocks[i].id == 3) {
+                motif3 = true;
+            }
+            // Example of accessing block fields:
+            // blocks[i].x, blocks[i].y, blocks[i].width, blocks[i].height, blocks[i].id
+        }
+
+        telemetry.update();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeLiftCameraDecode.java
@@ -0,0 +1,130 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.dfrobot.HuskyLens;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+
+import java.util.concurrent.TimeUnit;
+
+public class IntakeLiftCameraDecode {
+    /*
+    TODO: 1 motor for intke
+    TODO: 1 CR servo for sorter
+    TODO: 1 servo for transfer wheels
+
+    TODO: Turret: 3 Motors
+    TODO: - one for pitch, one for rotation, one for shooting (wheels)
+     */
+    
+    DcMotor intakeMotor;
+    DcMotor pitchMotor;
+    DcMotor rotationMotor;
+    DcMotor shootingMotor;
+    
+    CRServo transferServo;
+    CRServo sorterServo;
+
+    ColorSensor colorSensor;
+    
+    public void initILC(HardwareMap hwMap) {
+        intakeMotor = hwMap.get(DcMotor.class, "intakeMotor");
+        pitchMotor = hwMap.get(DcMotor.class, "pitchMotor");
+        rotationMotor = hwMap.get(DcMotor.class, "rotationMotor");
+        shootingMotor = hwMap.get(DcMotor.class, "shootingMotor");
+        colorSensor = hwMap.get(ColorSensor.class,"colorSensor");
+
+        rotationMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        pitchMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rotationMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        
+    }
+    
+    public void IntakeOn(){
+        intakeMotor.setPower(1);
+    }
+
+    public void IntakeOff(){
+        intakeMotor.setPower(0);
+    }
+
+    public void VerticalTurretManualMove(String direction) {
+        int Pos = pitchMotor.getCurrentPosition();
+
+        if(direction.equals("up")) {
+            Pos += 100;
+        }
+        else if(direction.equals("down")) {Pos -= 100;
+        }
+
+        pitchMotor.setTargetPosition(Pos);
+        pitchMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        pitchMotor.setPower(1);
+
+        if (pitchMotor.getCurrentPosition() == Pos) {
+            pitchMotor.setPower(0);
+        }
+    }
+
+    public void LateralTurretManualMove(String direction) {
+        int Pos = rotationMotor.getCurrentPosition();
+
+        if(direction.equals("left")) {
+            Pos += 100;
+        }
+        else if(direction.equals("right")) {Pos -= 100;
+        }
+
+        rotationMotor.setTargetPosition(Pos);
+        rotationMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rotationMotor.setPower(1);
+
+        if (rotationMotor.getCurrentPosition() == Pos) {
+            rotationMotor.setPower(0);
+        }
+    }
+
+    public void TransferOn() {
+        transferServo.setPower(1);
+    }
+
+    public void TransferOff() {
+        transferServo.setPower(0);
+    }
+
+    public void SorterOn() {
+        sorterServo.setPower(1);
+    }
+
+    public void SorterOff() {
+        sorterServo.setPower(0);
+    }
+
+    public void ShootingOn(){
+        shootingMotor.setPower(1);
+    }
+
+    public void ShootingOff(){
+        shootingMotor.setPower(0);
+    }
+
+    public boolean detectGreen() {
+        if (colorSensor.green() == 150) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean detectPurple() {
+        if (colorSensor.red() >= 140 && colorSensor.blue() >= 125) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
