fcaf856271a51f3f161e57d50d7a3a8a1ebf582c
Sumaedh <sumaedhk1@gmail.com>
2025-11-14 23:59:17 -0500

    Added Comment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveAndIntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveAndIntakeTest.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -11,30 +10,25 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import org.firstinspires.ftc.robotcore.internal.system.Deadline;
-
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import java.util.concurrent.TimeUnit;
 
 @Config
 @TeleOp(name = "DriveAndIntakeTest")
 public class DriveAndIntakeTest extends OpMode {
-
-
     // lever pos 0 as up and 0.123 and the bottom
     // pitch innit pos should be 0.5
     final int READ_PERIOD = 1;
-
     // TODO: find lever pos
     DcMotor intakeMotor;
     Servo pitchServo;
     DcMotor rotationMotor;
     DcMotor shootingMotor;
-
     DcMotor transferMotor;
     CRServo sorterServo;
     Servo leverServo;
-
     ColorSensor colorSensor;
     HuskyLens huskyLens;
     HuskyLens huskyLens2;
@@ -43,10 +37,8 @@ public class DriveAndIntakeTest extends OpMode {
     DcMotor frontRight;
     DcMotor backLeft;
     DcMotor backRight;
-    BNO055IMU turretImu;
-
+    IMU turretImu;
     ElapsedTime timer = new ElapsedTime();
-  
     public static double leverPos = 0;
     public static double pitchPos = 0;
 
@@ -58,28 +50,21 @@ public class DriveAndIntakeTest extends OpMode {
         backRight.setPower((left_y - left_x + right_x) / maxPower);
     }
 
-    /*
-    public void dPadMove(String direction) {
+    /* public void dPadMove(String direction) {
         int pos = rotationMotor.getCurrentPosition();
-
         if(direction.equals("up")) {
             pos += 75;
-        }
-        else if(direction.equals("down") && pos > 75) {
+        } else if(direction.equals("down") && pos > 75) {
             pos -= 75;
         }
-
         rotationMotor.setTargetPosition(pos);
         rotationMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rotationMotor.setPower(0.75);
-
         if (rotationMotor.getCurrentPosition() == pos) {
             rotationMotor.setPower(0);
         }
-    }
+    } */
 
-     */
-    
     @Override
     public void init() {
         intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
@@ -88,57 +73,47 @@ public class DriveAndIntakeTest extends OpMode {
         shootingMotor = hardwareMap.get(DcMotor.class, "shootingMotor");
         huskyLens = hardwareMap.get(HuskyLens.class, "huskylens");
         huskyLens2 = hardwareMap.get(HuskyLens.class, "huskylens2");
-
         transferMotor = hardwareMap.get(DcMotor.class, "transferMotor");
         sorterServo = hardwareMap.get(CRServo.class, "sorterServo");
         leverServo = hardwareMap.get(Servo.class,"leverServo");
-       // turretImu = hardwareMap.get(BNO055IMU.class, "turretImu");
+        turretImu = hardwareMap.get(IMU.class, "turretImu");
 
-       // BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-       // parameters.angleUnit           = BNO055IMU.AngleUnit.RADIANS;
-      //  parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        // Set up parameters for turret orientation (adjust based on mounting)
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+        );
 
-        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
-        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
-        // and named "imu".
-       // turretImu.initialize(parameters);
+        // Initialize
+        turretImu.initialize(new IMU.Parameters(orientationOnRobot));
 
         rotationMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rotationMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
         // Rate limiter for telemetry
         rateLimit = new Deadline(READ_PERIOD, TimeUnit.SECONDS);
         rateLimit.expire();
-
         leverServo.setPosition(0);
-
         // Choose the algorithm
         huskyLens.selectAlgorithm(HuskyLens.Algorithm.TAG_RECOGNITION);
         // huskyLens.selectAlgorithm(HuskyLens.Algorithm.COLOR_RECOGNITION);
-
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
-
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
         frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-
         intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         pitchServo.setDirection(Servo.Direction.REVERSE);
-
         frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
         frontLeft.setPower(0);
         frontRight.setPower(0);
         backLeft.setPower(0);
@@ -148,59 +123,43 @@ public class DriveAndIntakeTest extends OpMode {
     @Override
     public void loop() {
         driveMecanum(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-
         if (gamepad1.a) {
             intakeMotor.setPower(1);
         }
         if (gamepad1.b) {
             intakeMotor.setPower(0);
         }
-
-        /*
-        if (gamepad1.dpad_left) {
+        /* if (gamepad1.dpad_left) {
             dPadMove("up");
         }
         if (gamepad1.dpad_right) {
             dPadMove("down");
-        }
-
-         */
-
+        } */
         if (gamepad1.x) {
             shootingMotor.setPower(1);
         }
         if (gamepad1.y) {
             shootingMotor.setPower(0);
         }
-
         if (gamepad1.left_bumper) {
             transferMotor.setPower(1);
         }
         if (gamepad1.right_bumper) {
             transferMotor.setPower(-1);
         }
-
-        /*
-        leverServo.setPosition(leverPos);
-        pitchServo.setPosition(pitchPos);
-
-         */
+        /* leverServo.setPosition(leverPos);
+        pitchServo.setPosition(pitchPos); */
         // 0.45
-
         if (gamepad1.dpad_up) {
             leverServo.setPosition(0);
         }
         if (gamepad1.dpad_down) {
             leverServo.setPosition(0.16);
         }
-
-        /*
-        if (gamepad1.dpad_left) {
+        /* if (gamepad1.dpad_left) {
             shootingMotor.setPower(1);
             transferMotor.setPower(1);
             leverServo.setPosition(0);
-        }
-
-         */
+        } */
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewestSorting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewestSorting.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -10,9 +9,9 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import org.firstinspires.ftc.robotcore.internal.system.Deadline;
-
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import java.util.concurrent.TimeUnit;
 
 @Config
@@ -23,22 +22,18 @@ public class NewestSorting extends OpMode {
     Servo pitchServo;
     DcMotor rotationMotor;
     DcMotor shootingMotor;
-
     DcMotor transferMotor;
     CRServo sorterServo;
     Servo leverServo;
-
     ColorSensor colorSensor;
     HuskyLens huskyLens;
-
     HuskyLens huskyLens2;
-    BNO055IMU turretImu;
+    IMU turretImu;
     Deadline rateLimit;
     public static int encoderAmount = 0;
-
     boolean detected = false;
-
     ElapsedTime timer = new ElapsedTime();
+
     @Override
     public void init() {
         intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
@@ -48,66 +43,54 @@ public class NewestSorting extends OpMode {
         colorSensor = hardwareMap.get(ColorSensor.class,"colorSensor");
         huskyLens = hardwareMap.get(HuskyLens.class, "huskylens");
         huskyLens2 = hardwareMap.get(HuskyLens.class, "huskylens2");
-       // turretImu = hardwareMap.get(BNO055IMU.class, "turretImu");
+        turretImu = hardwareMap.get(IMU.class, "turretImu");
 
-      //  BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-      //  parameters.angleUnit           = BNO055IMU.AngleUnit.RADIANS;
-      //  parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        // Set up parameters for turret orientation (adjust based on mounting)
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+        );
 
-        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
-        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
-        // and named "imu".
-       // turretImu.initialize(parameters);
+        // Initialize
+        turretImu.initialize(new IMU.Parameters(orientationOnRobot));
 
         intakeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         intakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         transferMotor = hardwareMap.get(DcMotor.class, "transferMotor");
         sorterServo = hardwareMap.get(CRServo.class, "sorterServo");
         leverServo = hardwareMap.get(Servo.class,"leverServo");
-
         rotationMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rotationMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
         // Rate limiter for telemetry
         rateLimit = new Deadline(READ_PERIOD, TimeUnit.SECONDS);
         rateLimit.expire();
-
         leverServo.setPosition(0);
-
         // Choose the algorithm
         huskyLens.selectAlgorithm(HuskyLens.Algorithm.TAG_RECOGNITION);
         huskyLens2.selectAlgorithm(HuskyLens.Algorithm.OBJECT_RECOGNITION);
         // huskyLens.selectAlgorithm(HuskyLens.Algorithm.COLOR_RECOGNITION);
         timer.reset();
-
-
     }
 
     @Override
     public void loop() {
-
         if (!rateLimit.hasExpired()) {
             return;
         }
         rateLimit.reset();
-
         // Get blocks (recognized objects)
         HuskyLens.Block[] blocks = huskyLens2.blocks();
         telemetry.addData("Block count", blocks.length);
-
         int currentPos = intakeMotor.getCurrentPosition();
         int newPos = currentPos + encoderAmount;
-
         if (gamepad1.a) {
             while (intakeMotor.getCurrentPosition() <= newPos) {
                 sorterServo.setPower(0.5);
             }
         }
-
         if (detectGreen() || detectPurple()) {
             telemetry.addLine(" Ready to transfer!");
         }
-
         if (gamepad1.b) {
             for (int i = 0; i < blocks.length; i++) {
                 telemetry.addData("Block", blocks[i].toString());
@@ -121,12 +104,7 @@ public class NewestSorting extends OpMode {
                 // blocks[i].x, blocks[i].y, blocks[i].width, blocks[i].height, blocks[i].id
             }
         }
-
-
         telemetry.addData("encoder ticks", intakeMotor.getCurrentPosition());
-
-
-
     }
 
     public boolean detectGreen() {
